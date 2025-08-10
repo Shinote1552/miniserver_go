46f06bf3060856f14c6ef4369a08944e56167a7e
Edmure Pizza <edmurepi@un-c5.kzn.21-school.ru>
2025-08-10 16:08:36 +0300

    lneed fix main, inmemory is not correctly wotking

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -22,17 +22,38 @@ func main() {
 	ctxRoot := context.Background()
 	cfg := config.NewConfig()
 	log := logger.NewLogger()
+	var service *services.URLShortener
 
-	storage := initStorage(ctxRoot, log, *cfg)
-	defer closeStorage(log, storage)
+	if cfg.DatabaseDSN != "" {
+		storage, err := initPostgres(ctxRoot, log, cfg.DatabaseDSN)
+		if err != nil {
+			log.
+				Error().
+				Err(err).
+				Msg("Failed to initialize PostgreSQL storage")
+		}
+		defer closePostgresStorage(log, storage)
+		defer savePostgresData(ctxRoot, log, *cfg, storage)
+		initPostgresData(ctxRoot, log, *cfg, storage)
+		service = services.NewServiceURLShortener(storage, cfg.BaseURL)
+	} else {
+		storage := initInMemory(log)
+		defer closeInMemoryStorage(log, storage)
+		defer saveInMemoryData(ctxRoot, log, *cfg, storage)
+		initInMemoryData(ctxRoot, log, *cfg, storage)
+		service = services.NewServiceURLShortener(storage, cfg.BaseURL)
+	}
 
-	initData(ctxRoot, log, *cfg, storage)
-	defer saveData(ctxRoot, log, *cfg, storage)
-	service := services.NewServiceURLShortener(storage, cfg.BaseURL)
-
+	if service == nil {
+		log.
+			Fatal().
+			Msg("URL shortener service initialization failed")
+		return
+	}
 	srv, err := server.NewServer(log, *cfg, service)
 	if err != nil {
-		log.Fatal().
+		log.
+			Fatal().
 			Err(err).
 			Msg("Failed to create server")
 	}
@@ -47,7 +68,8 @@ func runServer(srv *server.Server, log *zerolog.Logger) {
 
 	go func() {
 		if err := srv.Start(context.Background()); err != nil && err != http.ErrServerClosed {
-			log.Fatal().
+			log.
+				Fatal().
 				Err(err).
 				Msg("Server failed to start")
 		}
@@ -55,7 +77,8 @@ func runServer(srv *server.Server, log *zerolog.Logger) {
 
 	// Shutdown, TODO gracefull shutdown
 	sig := <-stop
-	log.Info().
+	log.
+		Info().
 		Str("signal", sig.String()).
 		Msg("Received signal, shutting down server...")
 
@@ -63,88 +86,160 @@ func runServer(srv *server.Server, log *zerolog.Logger) {
 	defer cancel()
 
 	if err := srv.Shutdown(ctx); err != nil {
-		log.Error().Err(err).Msg("Server shutdown error")
-	} else {
-		log.Info().Msg("Server shutdown completed successfully")
-	}
-}
-
-func initPostgres(ctx context.Context, log *zerolog.Logger, cfg config.Config) *postgres.PostgresStorage {
-	if cfg.DatabaseDSN != "" {
-		storage, err := postgres.NewStorage(ctx, cfg.DatabaseDSN)
-		if err != nil {
-			log.Error().
-				Err(err).
-				Msg("Failed to initialize PostgreSQL storage")
-
-			log.Info().
-				Msg("Falling back to in-memory storage")
-			storage := inmemory.NewStorage()
-			return storage
-		}
-
-		log.Info().
-			Msg("Using PostgreSQL storage")
-		return storage
-	}
-
-	log.Info().
-		Msg("Using in-memory storage")
-	storage := inmemory.NewStorage()
-	return storage
-}
-
-func initInMemory() *inmemory.InmemoryStorage {
-
-}
-
-func closeStorage(log *zerolog.Logger, storage repository.Storage) {
-	if err := storage.Close(); err != nil {
-		log.Error().
+		log.
+			Error().
 			Err(err).
-			Msg("Failed to close storage")
+			Msg("Server shutdown error")
+	} else {
+		log.
+			Info().
+			Msg("Server shutdown completed successfully")
+	}
+}
+
+func initPostgres(ctx context.Context, log *zerolog.Logger, dsn string) (*postgres.PostgresStorage, error) {
+	storage, err := postgres.NewStorage(ctx, dsn)
+	if err != nil {
+		log.
+			Error().
+			Err(err).
+			Msg("Failed to initialize PostgreSQL storage")
+		return nil, err
+	}
+
+	log.
+		Info().
+		Msg("Using PostgreSQL storage")
+	return storage, nil
+}
+
+func initInMemory(log *zerolog.Logger) *inmemory.InmemoryStorage {
+	log.
+		Info().
+		Msg("Using in-memory storage")
+	return inmemory.NewStorage()
+}
+
+func closePostgresStorage(log *zerolog.Logger, storage *postgres.PostgresStorage) {
+	if err := storage.Close(); err != nil {
+		log.
+			Error().
+			Err(err).
+			Msg("Failed to close PostgreSQL storage")
 		return
 	}
-	log.Info().
-		Msg("Storage closed successfully")
+	log.
+		Info().
+		Msg("PostgreSQL storage closed successfully")
 }
 
-func initData(ctx context.Context, log *zerolog.Logger, cfg config.Config, storage repository.Storage) {
+func closeInMemoryStorage(log *zerolog.Logger, storage *inmemory.InmemoryStorage) {
+	if err := storage.Close(); err != nil {
+		log.
+			Error().
+			Err(err).
+			Msg("Failed to close in-memory storage")
+		return
+	}
+	log.
+		Info().
+		Msg("In-memory storage closed successfully")
+}
+
+// Для PostgreSQL хранилища
+func initPostgresData(ctx context.Context, log *zerolog.Logger, cfg config.Config, storage *postgres.PostgresStorage) {
 	if cfg.FileStoragePath == "" {
-		log.Info().Msg("No file storage path specified, skip loading data")
+		log.
+			Info().
+			Msg("No file storage path specified, skip loading data to PostgreSQL")
 		return
 	}
 
 	path, err := filestore.Load(ctx, *log, cfg.FileStoragePath, storage)
 	if err != nil {
-		log.Error().
+		log.
+			Error().
 			Err(err).
 			Str("path", cfg.FileStoragePath).
-			Msg("Failed to load data from file")
+			Msg("Failed to load data from file to PostgreSQL")
 		return
 	}
 
-	log.Info().
+	log.
+		Info().
 		Str("path", path).
-		Msg("Data loaded successfully from file")
+		Msg("Data loaded successfully from file to PostgreSQL")
 }
 
-func saveData(ctx context.Context, log *zerolog.Logger, cfg config.Config, storage repository.Storage) {
+func savePostgresData(ctx context.Context, log *zerolog.Logger, cfg config.Config, storage *postgres.PostgresStorage) {
 	if cfg.FileStoragePath == "" {
-		log.Info().Msg("No file storage path specified, skip saving data")
+		log.
+			Info().
+			Msg("No file storage path specified, skip saving PostgreSQL data")
 		return
 	}
 
 	path, err := filestore.Save(ctx, log, cfg.FileStoragePath, storage)
 	if err != nil {
-		log.Error().
+		log.
+			Error().
 			Err(err).
 			Str("path", path).
-			Msg("Failed to save data to file")
+			Msg("Failed to save PostgreSQL data to file")
 		return
 	}
 
-	log.Info().
+	log.
+		Info().
 		Str("path", path).
-		Msg("Data saved successfully to file")
+		Msg("PostgreSQL data saved successfully to file")
+}
+
+// Для InMemory хранилища
+func initInMemoryData(ctx context.Context, log *zerolog.Logger, cfg config.Config, storage *inmemory.InmemoryStorage) {
+	if cfg.FileStoragePath == "" {
+		log.
+			Info().
+			Msg("No file storage path specified, skip loading data to in-memory storage")
+		return
+	}
+
+	path, err := filestore.Load(ctx, *log, cfg.FileStoragePath, storage)
+	if err != nil {
+		log.
+			Error().
+			Err(err).
+			Str("path", cfg.FileStoragePath).
+			Msg("Failed to load data from file to in-memory storage")
+		return
+	}
+
+	log.
+		Info().
+		Str("path", path).
+		Msg("Data loaded successfully from file to in-memory storage")
+}
+
+func saveInMemoryData(ctx context.Context, log *zerolog.Logger, cfg config.Config, storage *inmemory.InmemoryStorage) {
+	if cfg.FileStoragePath == "" {
+		log.
+			Info().
+			Msg("No file storage path specified, skip saving in-memory data")
+		return
+	}
+
+	path, err := filestore.Save(ctx, log, cfg.FileStoragePath, storage)
+	if err != nil {
+		log.
+			Error().
+			Err(err).
+			Str("path", path).
+			Msg("Failed to save in-memory data to file")
+		return
+	}
+
+	log.
+		Info().
+		Str("path", path).
+		Msg("In-memory data saved successfully to file")
 }
